03fbeea12cf9943a2c0b756859198f5f599606ce
Christ Komika <christkomika7@gmail.com>
2023-05-12 11:10:03 +0100

    Corrections bug

--- server/routes/auth.route.ts
+++ server/routes/auth.route.ts
@@ -7,5 +7,4 @@ routes.post("/auth/login", AuthController.login);
 routes.post("/auth/refresh", AuthController.refreshToken);
 routes.post("/auth/operator", AuthController.getOperator);
 routes.delete("/auth/logout", AuthController.logout);
-
 export default routes;
